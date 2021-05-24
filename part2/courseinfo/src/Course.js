a3d15fbad68d985cc0572fee18ce8bc668ae19c0
Jack John <jjohn05@mylangara.ca>
2021-05-23 19:27:32 -0700

    complete step 7

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -5,12 +5,21 @@ const Header = ({ content }) =>  <h1>{content}</h1>
 const Part = ({parts}) => <p>{parts.name} {parts.exercises} </p>    
     
 const Content = ({ parts }) => <div>{parts.map(part => <Part key={part.id} parts={part} />)}</div>
+
+const Total = ({ parts }) => {
+    let sum = 0
+    parts.map((part => sum += part.exercises))
+    return(
+      <p>Number of exercises {sum}</p>
+    ) 
+  }
     
 const Course = ({course}) => {
     return (
         <>
         <Header key={course.id} content={course.name} />
         <Content parts={course.parts} />
+        <Total parts={course.parts} />
         </>
     )
 }
