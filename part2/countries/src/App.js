0434a6498e81dad79d947fd34f5d5fb19a8c440e
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-10 21:33:00 -0800

    Add code for Step 2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 
 const DisplayCountry = ({countryInfo}) => {
-  const info = countryInfo[0]
+  const info = Array.isArray(countryInfo) ? countryInfo[0] : countryInfo
   return (
     <>
     <h2>{info.name.common}</h2>
@@ -21,11 +21,34 @@ const DisplayCountry = ({countryInfo}) => {
   )
   }
 
+  const Button = ({country}) => {
+    const [onClick, setOnClick] = useState(false)
+    return (
+      <>
+      <button onClick={() => setOnClick(!onClick)}>Show</button>
+      {onClick && <DisplayCountry countryInfo={country} />}
+      </>
+    )
+  }
+
+  const DisplayCountryList = ({countriesList, country}) => {
+     return (
+      countriesList.map((item, index) => (
+      <li style={{listStyle: 'none'}} key={index}>
+      {item.name.common}&nbsp;&nbsp;
+      <Button country={item}/>
+      <br />
+      </li>
+      )))}
+    
+
+
 export default function App() {
 
   
   const [input, setInput] = useState("")
   const [countries, setCountries]  = useState([])
+  const [onShow, setOnShow] = useState(false)
 
   const onChangeHandler = (e) => {
    setInput(e.target.value);
@@ -42,14 +65,15 @@ export default function App() {
    }).catch(err => err)}
   }, [input])
 
+  const onShowHandler = () => setOnShow(!onShow)
   let countriesLength = Array.isArray(countries) ? countries.length : 1;
   return (
   <>
       <p>find countries 
         <input type="text" value={input} onChange={onChangeHandler}/>
         <br />
-         {(countries.length > 1 && countries.length <= 10) && 
-          countries.map((item, index) => <li style={{listStyle: 'none'}} key={index}>{item.name.common}<br /></li>)} 
+        {(countries.length > 1 && countries.length <= 10) && 
+        <DisplayCountryList countriesList={countries} />}
           </p>
            {countries.length === 1 && <DisplayCountry countryInfo={countries}/>}
            {(input && countriesLength > 10) && "Too many matches, specify another filter"}
