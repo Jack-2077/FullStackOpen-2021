733f799cbd4c4c3f6c3014f3890d93eafa34f933
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-12 12:14:49 -0800

    Complete step 9

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,98 +1,112 @@
-import "./styles.css";
-import {useEffect, useState} from "react"
-import axios from "axios"
+import React, { useState } from 'react';
+import Persons from './components/Persons';
+import personService from './services/persons';
 
+const Header = ({ content }) => <h2>{content}</h2>;
 
-const DisplayCountry = ({countryInfo}) => {
-  const [weather, setWeather] = useState({temp: "", png: "", wind: "", dir: ""})
-  const info = Array.isArray(countryInfo) ? countryInfo[0] : countryInfo
+const Form = ({ content, value, onChange }) => (
+  <div>
+    {content} <input value={value} onChange={onChange} />
+  </div>
+);
 
-  axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_YOUR_API_KEY_NAME}&q=${info.capital}&aqi=no`)
-  .then(response => {
-    setWeather({temp: response.data.current.temp_c, png: response.data.current.condition.icon,
-    wind: response.data.current.wind_mph, dir: response.data.current.wind_dir})
-  }).catch(error => console.log(error))
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filterName, setNewFilterName] = useState('');
+  const [filterPersons, setFilterPersons] = useState({ name: '', number: '' });
+
+  useState(() => {
+    personService
+      .getAll()
+      .then((intialPersonsList) => setPersons(intialPersonsList));
+  }, []);
+
+  const addName = () => {
+    const newPersonObject = {
+      name: newName,
+      number: newNumber,
+    };
+
+    personService.create(newPersonObject).then((personsList) => {
+      setPersons(persons.concat(personsList));
+      setNewNumber('');
+      setNewName('');
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newName && newNumber) {
+      persons.some((item) => item.name === newName || item.number === newNumber)
+        ? alert(`this person or number is already added to phonebook`)
+        : addName();
+    } else {
+      alert('Your name or number is missing');
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const searchItem = (input) => {
+    persons.some((item) => {
+      if (item.name.toLowerCase() === input)
+        setFilterPersons({ name: item.name, number: item.number });
+      return 0;
+    });
+  };
+
+  const handleFilterChange = (event) => {
+    setFilterPersons({ name: '', number: '' });
+    setNewFilterName(event.target.value);
+    const input = event.target.value.trim().toLowerCase();
+    input.length > 0 && searchItem(input);
+  };
+
+  const onDeleteHandler = (id, name) => {
+    const message = `Delete ${name} ?`;
+    if (window.confirm(message)) {
+      personService.deletePerson(id).then((response) => response);
+      setPersons(persons.filter((p) => p.id !== id));
+    } else return 0;
+  };
+
   return (
-    
-    <>
-    
-    <h2>{info.name.common}</h2>
-    <p>capital {info.capital}</p>
-    <p>population {info.population} </p>
-
-    <h3>Languages</h3>
-    <ul>
-      {Object.values(info.languages).map((item, index) => 
-      <li key={index}>{item}</li>)}
-      </ul>
-      <img src={info.flags["png"]} alt="flag" height="100px" width="150px"/>
-      <h3>Weather in {info.name.common}</h3>
-      <strong>temperature: {weather.temp} Celsius</strong><br />
-      <img src={weather.png} alt="country flag" /><br />
-      <strong>wind:</strong> {weather.wind} mph direction {weather.dir}
-    </>
-  )
-  }
-
-  const Button = ({country}) => {
-    const [onClick, setOnClick] = useState(false)
-    return (
-      <>
-      <button onClick={() => setOnClick(!onClick)}>Show</button>
-      {onClick && <DisplayCountry countryInfo={country} />}
-      </>
-    )
-  }
-
-  const DisplayCountryList = ({countriesList}) => {
-     return (
-      countriesList.map((item, index) => (
-      <li style={{listStyle: 'none'}} key={index}>
-      {item.name.common}&nbsp;&nbsp;
-      <Button country={item}/>
-      <br />
-      </li>
-      )))}
-    
-
-
-export default function App() {
-
-  
-  const [input, setInput] = useState("")
-  const [countries, setCountries]  = useState([])
-
-  const onChangeHandler = (e) => {
-   setInput(e.target.value);
-  }
-
-  useEffect(() => {
-
-    if(input)
-    {
-    axios
-   .get(`https://restcountries.com/v3.1/name/${input}`)
-   .then(response => {
-     setCountries(response.data)
-   }).catch(err => err)}
-  }, [input])
-
-  let countriesLength = Array.isArray(countries) ? countries.length : 1;
-  return (
-  <>
-      <p>find countries 
-        <input type="text" value={input} onChange={onChangeHandler}/>
-        <br />
-        {(countries.length > 1 && countries.length <= 10) && 
-        <DisplayCountryList countriesList={countries} />}
-          </p>
-           {countries.length === 1 && <DisplayCountry countryInfo={countries}/>}
-           {(input && countriesLength > 10) && "Too many matches, specify another filter"}
-        
-       
-      
-       
-      
-  </>
+    <div>
+      <Header content='Phonebook' />
+      <Form
+        content='filter shown with '
+        value={filterName}
+        onChange={handleFilterChange}
+      />
+      <Header content='add a new' />
+      <form onSubmit={handleSubmit}>
+        <Form content='name: ' value={newName} onChange={handleNameChange} />
+        <Form
+          content='number: '
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <Header content='Numbers' />
+      {!filterName ? (
+        <Persons persons={persons} deleteHandler={onDeleteHandler} />
+      ) : (
+        filterPersons.name + ' ' + filterPersons.number
+      )}
+    </div>
   );
-}
+};
+
+export default App;
+
