f9a230b630d0dfafb5e74e95962b08f70daa9901
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-14 19:45:52 -0800

    Create person.js

---
+++ part2/phonebook/src/services/person.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/persons';
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newPerson) => {
+  const request = axios.post(baseUrl, newPerson);
+  return request.then((response) => response.data);
+};
+
+const update = (id, updatedPerson) => {
+  const request = axios.put(`${baseUrl}/${id}`, updatedPerson);
+  return request.then((response) => response.data);
+};
+
+const deletePerson = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+export default { create, getAll, deletePerson, update };
