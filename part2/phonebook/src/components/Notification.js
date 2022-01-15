a340b60fe707b795319e590e160575f371c502e3
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-01-14 19:43:55 -0800

    Update Notification.js

--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
-const styles = {
-  color: 'green',
-  background: 'lightgrey',
-  fontSize: '20px',
-  borderStyle: 'solid',
-  borderRadius: '5px',
-  padding: '10px',
-  marginNBottom: '10px',
-};
 const Notification = ({ message }) => {
-  if (!message) {
+  const styles = {
+    color: message.errorCode === 1 ? 'red' : 'green',
+    background: 'lightgrey',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginNBottom: '10px',
+  };
+
+  if (!message.message) {
     return null;
   }
 
-  return <div style={styles}>{message}</div>;
+  return <div style={styles}>{message.message}</div>;
 };
 
 export default Notification;
